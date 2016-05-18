b5bc44f48625195a8db77f57388fc10027f26961
NetDragoon <netdragoon@outlook.com>
2016-05-18 11:31:28 -0300

    add button size

---
+++ src/Canducci.Helpers/ButtonSize.cs
@@ -0,0 +1,22 @@
+﻿namespace Canducci.Helpers
+{
+    public enum ButtonSize
+    {
+        [EnumDescription()]
+        Default,
+        [EnumDescription("btn-sm")]
+        Small,
+        [EnumDescription("btn-xs")]
+        ExtraSmall,
+        [EnumDescription("btn-lg")]
+        Large,
+        [EnumDescription("btn-block")]
+        DefaultAndBlock,
+        [EnumDescription("btn-block btn-sm")]
+        SmallAndBlock,
+        [EnumDescription("btn-block btn-xs")]
+        ExtraSmallAndBlock,
+        [EnumDescription("btn-block btn-lg")]
+        LargeAndBlock
+    }
+}
--- src/Canducci.Helpers/ButtonSubmit.cs
+++ src/Canducci.Helpers/ButtonSubmit.cs
@@ -2,29 +2,52 @@
 using Microsoft.AspNetCore.Razor.TagHelpers;
 namespace Canducci.Helpers
 {
-    [HtmlTargetElement("button-submit", Attributes = ButtonLabel)]
-    [HtmlTargetElement("button-submit", Attributes = ButtonCss)]
-    [HtmlTargetElement("button-submit", Attributes = ButtonStyle)]
-    [HtmlTargetElement("button-submit", Attributes = ButtonGlyphicon)]
+    [HtmlTargetElement("button-submit", Attributes = cButtonLabel)]
+    [HtmlTargetElement("button-submit", Attributes = cButtonCss)]
+    [HtmlTargetElement("button-submit", Attributes = cButtonStyle)]
+    [HtmlTargetElement("button-submit", Attributes = cButtonGlyphicon)]
+    [HtmlTargetElement("button-submit", Attributes = cButtonSize)]
+    [HtmlTargetElement("button-submit", Attributes = cButtonDisabled)]
     public class ButtonSubmit : TagHelper
     {
-        protected const string ButtonLabel = "button-label";
-        protected const string ButtonCss = "button-css";
-        protected const string ButtonStyle = "button-style";
-        protected const string ButtonGlyphicon = "button-glyphicon";
+        protected const string cButtonLabel = "button-label";
+        protected const string cButtonCss = "button-css";
+        protected const string cButtonStyle = "button-style";
+        protected const string cButtonGlyphicon = "button-glyphicon";
+        protected const string cButtonSize = "button-size";
+        protected const string cButtonDisabled = "button-disabled";
 
-        [HtmlAttributeName(ButtonLabel)]
+        [HtmlAttributeName(cButtonLabel)]
         public string Label { get; set; } = "Default";
 
-        [HtmlAttributeName(ButtonCss)]
+        [HtmlAttributeName(cButtonCss)]
         public string Css { get; set; } = "";
 
-        [HtmlAttributeName(ButtonStyle)]
+        [HtmlAttributeName(cButtonStyle)]
         public ButtonBootstrapStyle Style { get; set; } = ButtonBootstrapStyle.None;
 
-        [HtmlAttributeName(ButtonGlyphicon)]
+        [HtmlAttributeName(cButtonGlyphicon)]
         public Glyphicon Glyphicon { get; set; } = Glyphicon.None;
 
+        [HtmlAttributeName(cButtonSize)]
+        public ButtonSize Size { get; set; } = ButtonSize.Default;
+
+        [HtmlAttributeName(cButtonDisabled)]
+        public bool Disabled { get; set; } = false;
+
+        protected void MergeClass(TagHelperOutput output, object value)
+        {
+            if (output.Attributes.ContainsName("class"))
+            {
+                output.Attributes.SetAttribute("class",
+                    output.Attributes["class"].Value + " " + value);
+            }
+            else
+            {
+                output.Attributes.Add("class", value);
+            }
+            
+        }
 
         protected void Render(TagHelperContext context, TagHelperOutput output)
         {
@@ -32,24 +55,11 @@ namespace Canducci.Helpers
             output.TagMode = TagMode.StartTagAndEndTag;
             output.Attributes.Add("type", "submit");
 
-            if (!string.IsNullOrEmpty(Css))
-            {
-                output.Attributes.Add("class", Css);
-            }
+            if (Disabled) output.Attributes.Add("disabled","disabled");
+            if (string.IsNullOrEmpty(Css) == false) MergeClass(output, Css);
+            if (Style != ButtonBootstrapStyle.None) MergeClass(output, EnumDescriptionItem.GetValue(Style));
+            if (Size != ButtonSize.Default) MergeClass(output, EnumDescriptionItem.GetValue(Size));
 
-            if (Style != ButtonBootstrapStyle.None)
-            {
-                if (output.Attributes.ContainsName("class"))
-                {
-                    output.Attributes.SetAttribute("class",
-                        output.Attributes["class"].Value + " " +
-                        EnumDescriptionItem.GetValue(Style));
-                }
-                else
-                {
-                    output.Attributes.Add("class", EnumDescriptionItem.GetValue(Style));
-                }
-            }
             if (Glyphicon == Glyphicon.None)
             {
                 output
